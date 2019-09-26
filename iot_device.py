d922d0daf14975e34b8fba3a63a7ae07f1724a72
Dimas YR <dimas.yoan.r@gmail.com>
2019-09-26 20:10:07 +0700

    nambah penghitungan waktu mining client

--- iot_device.py
+++ iot_device.py
@@ -1,10 +1,10 @@
 import socket, time
 import random, string, sys
 
-# TCP_IP = '10.60.101.137'
+TCP_IP = '10.60.103.30'
 
 # ip localhost
-TCP_IP = '127.0.0.1'
+# TCP_IP = '127.0.0.1'
 
 TCP_PORT_IOT = 8882
 
@@ -12,6 +12,8 @@ HEADERSIZE = 7
 
 def hexlify(s):
     return s.encode().hex()
+
+
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,28 +25,29 @@ sock.connect(server_address)
 
 msg = ''
 
-iot = True
+def iotmode(size):
+    for i in range(30):
+        hex_msg = hexlify(''.join([random.choice(string.ascii_letters) for _ in range(size)]))
+        hex_msg2 = f'{len(hex_msg):<{HEADERSIZE}}' + hex_msg
 
+        print('data hexa:', hex_msg, '\n')
+        print('ukuran data:', sys.getsizeof(hex_msg), 'bytes')
+
+        sock.sendall(hex_msg2.encode())
+
+        time.sleep(25)
 try:
-    # iot mode
-    if iot == True:
-        for i in range(10):
-            # 4 (33 bytes) 52 (129 bytes)
-            hex_msg = hexlify(''.join([random.choice(string.ascii_letters) for _ in range(52)]))
-            hex_msg2 = f'{len(hex_msg):<{HEADERSIZE}}' + hex_msg
-
-            print('data hexa:', hex_msg, '\n')
-            print('ukuran data:', sys.getsizeof(hex_msg), 'bytes')
-
-            sock.sendall(hex_msg2.encode())
-
-            time.sleep(30)
-
     # chat mode
     # 'end' untuk mengakhiri
     while msg != 'end':
         msg = input("Kirim pesan: ")
-        if msg != '' and msg != 'end':
+        if msg == 'iot32':
+            iotmode(4)
+
+        elif msg == 'iot128':
+            iotmode(52)
+
+        elif msg != '' and msg != 'end':
             try:
                 # random msg with number input
                 msg = int(msg)
--- mc_client.py
+++ mc_client.py
@@ -1,13 +1,14 @@
 import socket
 import threading
+import time
 from recordCPU import RecordCPU
 
 if __name__ == '__main__':
 
-    # TCP_IP = '10.60.101.137'
+    TCP_IP = '10.60.103.30'
 
     # ip localhost
-    TCP_IP = '127.0.0.1'
+    # TCP_IP = '127.0.0.1'
 
     TCP_PORT = 8881
     BUFFER_SIZE = 1024
@@ -29,12 +30,16 @@ if __name__ == '__main__':
             print('mining...')
             CPU.is_mining = True
             CPU.record_cpu = True
+            awal = time.time()
             threading.Thread(target=CPU.recordCpuUsage).start()
 
             msg = client_socket.recv(1024).decode('utf-8')
             if msg == 'done':
                 CPU.is_mining = False
                 CPU.record_cpu = False
+                akhir = time.time()
+                mining_time = akhir - awal
+                CPU.publish_time.append(round(mining_time,2))
                 print('done mining.')
 
         # print grafik cpu usage
@@ -50,7 +55,7 @@ if __name__ == '__main__':
         # menyimpan grafik cpu
         elif msg == 'save':
             CPU.printCpuUsage('publish', True)
-            CPU.printCpuUsage('search', True)
+            # CPU.printCpuUsage('search', True)
             print('figures saved')
 
         # menyimpan ke file biner
